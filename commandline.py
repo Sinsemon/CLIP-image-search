d3cbc3f409e55a755fb53ea99f352a61c9e393de
Simon Geupel <geupelsi@web.de>
2026-02-02 13:42:39 +0100

    add command line

---
+++ commandline.py
@@ -0,0 +1,35 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+
+class Arguments:
+    db_path:Path
+    search_string:str
+    create:Path
+    update:bool
+
+def parse() -> Arguments:
+
+    parent_parser = ArgumentParser(add_help=False)
+    parent_parser.add_argument("-db", "--db-path", type=Path, required=True)
+
+    parser = ArgumentParser(
+        prog="CLIP Image Search",
+        description="Search for images using CLIP AI model."
+    )
+    subparsers = parser.add_subparsers()
+    search_parser = subparsers.add_parser("search", help="Search in an existing database for images.", parents=[parent_parser])
+    manage_parser = subparsers.add_parser("manage", help="Create or update an existing database.", parents=[parent_parser])
+
+    search_parser.add_argument("search_string")
+    mng_group = manage_parser.add_mutually_exclusive_group(required=True)
+    mng_group.add_argument("--create", type=Path, help="Create a new database with the given image folder as root. All images under the folder will be embedded.")
+    mng_group.add_argument("--update", action="store_true", help="Insert new images into the database and remove nonexistent images.")
+
+    return parser.parse_args(namespace=Arguments())
+
+
+# search with existing:     main.py search -db /path/to/db "Ein Bild von ..."
+# create db:                main.py manage -db /path/to/db --create /path/to/images
+# clean db:                 main.py manage -db /path/to/db --update
--- test.py
+++ test.py
@@ -1,25 +1,51 @@
 from database import Database
+from commandline import parse
 
 from unittest import TestCase
 import unittest
+import sys
+from pathlib import Path
 
 import torch
 
 
-class TestDatabase(TestCase):
-    db_save_path = "test_files/db"
-    images = "test_files/images"
+# class TestDatabase(TestCase):
+#     db_save_path = "test_files/db"
+#     images = "test_files/images"
 
-    db:Database
+#     db:Database
     
-    def setUp(self) -> None:
-        super().setUp()
-        self.db = Database(self.db_save_path)
+#     def setUp(self) -> None:
+#         super().setUp()
+#         self.db = Database(self.db_save_path)
 
-    def tearDown(self) -> None:
-        return super().tearDown()
+#     def tearDown(self) -> None:
+#         return super().tearDown()
     
+class TestCommandline(TestCase):
+    # search with existing:     main.py search -db /path/to/db "Ein Bild von ..."
+    # create db:                main.py manage -db /path/to/db --create /path/to/images
+    # clean db:                 main.py manage -db /path/to/db --update
     
+    def test_search(self):
+        sys.argv = ["/blah/main.py", "search", "-db", "/path/to/db/db.csv", "Ein Bild von ..."]
+        args = parse()
+        self.assertEqual(args.db_path, Path("/path/to/db/db.csv"))
+        self.assertEqual(args.search_string,  "Ein Bild von ...")
+    
+    def test_create(self):
+        sys.argv = ["/blah/main.py", "manage", "-db", "/path/to/db/db.csv", "--create", "/path/to/images"]
+        args = parse()
+        self.assertEqual(args.db_path, Path("/path/to/db/db.csv"))
+        self.assertEqual(args.create, Path("/path/to/images"))
+        self.assertFalse(args.update)
+
+    def test_update(self):
+        sys.argv = ["/blah/main.py", "manage", "-db", "/path/to/db/db.csv", "--update"]
+        args = parse()
+        self.assertEqual(args.db_path, Path("/path/to/db/db.csv"))
+        self.assertEqual(args.update, True)
+
 
 
 if __name__ == "__main__":
